65a61a8aefeabd3b5e7f37ac356572093368879a
Jonathan <5377074j@gmail.com>
2021-11-01 00:00:56 +0200

    Fix: allow macros with more than one token

--- jolang/preprocessor/preprocessor.py
+++ jolang/preprocessor/preprocessor.py
@@ -9,20 +9,12 @@ def preprocess(stream: typing.Iterator[tokens.Token]):
             try:
                 macro = next(stream)
                 if macro.content == "macro":
-                    replace, replace_with = next(stream), [next(stream)]
-                    assert replace_with != [tokens.Newline]
-                    if isinstance(replace_with[0], tokens.LeftParen):
-                        replace_with.pop()
-                        while not isinstance(tok := next(stream), tokens.RightParen):
-                            replace_with.append(tok)
-                    newline = next(stream)
-                    if not isinstance(newline, tokens.Newline):
-                        raise SyntaxError("A macro should be ended with a newline [{}:{}]".format(macro.line, macro.col - 1))
+                    replace, replace_with = next(stream), []
+                    while not isinstance(tok := next(stream), tokens.Newline):
+                        replace_with.append(tok)
                     macros[replace.name, replace.content] = replace_with
             except StopIteration:
-                raise SyntaxError("A macro was never closed [line {}]".format(macro.line)) from None
-            except AssertionError:
-                macros[replace.name, replace.content] = []
+                break
         else:
             if (macro := macros.get((token.name, token.content))) is not None:
                 yield from macro
