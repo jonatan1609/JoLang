457b08b7cbd9ed60a6b83ed8b6e0f4574799821d
Jonathan <5377074j@gmail.com>
2021-11-04 22:11:56 +0200

    Add: add support for macros in the shell

--- jolang/shell.py
+++ jolang/shell.py
@@ -8,8 +8,10 @@ from jolang.parser import Parser, ast
 class Evaluate:
     PATTERN = re.compile(r'(?=[A-Z])')
 
-    def __init__(self, node: ast.Body):
+    def __init__(self, node: ast.Body = None):
         self.node = node
+        self.variables = {}
+        self.macros = {}
 
     @staticmethod
     def pascal_case_to_snake_case(string: str):
@@ -18,6 +20,12 @@ class Evaluate:
     @staticmethod
     def visit_number(node: ast.Number):
         return node.argument
+
+    def visit_constant(self, v: ast.Constant):
+        var = self.variables.get(v.argument)
+        if not var:
+            raise NameError(f"Variable {v.argument!r} does not exist!")
+        return var
 
     def visit_binary_node(self, node: ast.BinaryNode):
         left, right = self._visit(node.left), self._visit(node.right)
@@ -38,19 +46,32 @@ class Evaluate:
         method = getattr(self, method)
         return method(node)
 
+    @staticmethod
+    def visit_string(node):
+        return node.argument
+
     def visit(self):
         if self.node.statements:
             return self._visit(self.node.statements[0])
         return ''
 
 
+evaluator = Evaluate()
+
+
 def main(code: str):
     stream = Tokenizer(code).tokenize()
-    return Evaluate(Parser(preprocess(stream)).parse()).visit()
+    preprocessor = preprocess(stream, evaluator.macros)
+    parser = Parser(preprocessor)
+    evaluator.node = parser.parse()
+    evaluator.macros.update(parser.macros)
+    return evaluator.visit()
 
 
 try:
     while (command := input('JoLang >>> ')).lower() not in ('quit', 'exit'):
-        print(main(command))
+        res = main(command)
+        if res:
+            print(res)
 except KeyboardInterrupt:
     pass
