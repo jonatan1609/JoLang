f90ed2099ea7e16ab933ed85758534e05f71e3c7
Jonathan <5377074j@gmail.com>
2021-11-07 20:12:20 +0200

    Fix: fix preprocessor, allow only identifiers as replacements

--- jolang/preprocessor/preprocessor.py
+++ jolang/preprocessor/preprocessor.py
@@ -3,6 +3,7 @@ from ..tokenizer import tokens
 
 
 def preprocess(stream: typing.Iterator[tokens.Token], macros=None):
+    # Macro: '%' Identifier Expr
     if not macros:
         macros = {}
     for token in stream:
@@ -11,11 +12,18 @@ def preprocess(stream: typing.Iterator[tokens.Token], macros=None):
                 macro = next(stream)
                 if macro.content == "macro":
                     replace, replace_with = next(stream), []
+                    assert isinstance(replace, tokens.Identifier), "A macro replace must be an identifier!"
                     while not isinstance(tok := next(stream, tokens.Newline), tokens.Newline):
                         replace_with.append(tok)
                     macros[replace.name, replace.content] = replace_with
+                else:
+                    raise SyntaxError(f"Couldn't process a macro at line {token.line}")
             except StopIteration:
-                break
+                try:
+                    macro
+                    replace
+                except UnboundLocalError:
+                    raise SyntaxError(f"Couldn't process a macro at line {token.line}")
         else:
             if (macro := macros.get((token.name, token.content))) is not None:
                 yield from macro
