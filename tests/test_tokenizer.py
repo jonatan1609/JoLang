000072d0fc66bfa6fd0a4df62abf3d7f5e4131a7
Jonathan <5377074j@gmail.com>
2021-11-25 02:37:50 +0200

    Enhance: add tests for comments

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -12,7 +12,9 @@ class TestTokenizer(TestCase):
         "||": [tokens.LogicOr],
         "abc a0 01": [tokens.Identifier, tokens.Identifier, tokens.Integer],
         "0x222 0o222 2.2": [tokens.Integer, tokens.Integer, tokens.Float],
-        "func a(){return a % 2 - 1 == 2}": [tokens.Identifier, tokens.Identifier, tokens.LeftParen, tokens.RightParen, tokens.LeftBrace, tokens.Identifier, tokens.Identifier, tokens.Modulo, tokens.Integer, tokens.Subtract, tokens.Integer, tokens.IsEqual, tokens.Integer, tokens.RightBrace]
+        "func a(){return a % 2 - 1 == 2}": [tokens.Identifier, tokens.Identifier, tokens.LeftParen, tokens.RightParen, tokens.LeftBrace, tokens.Identifier, tokens.Identifier, tokens.Modulo, tokens.Integer, tokens.Subtract, tokens.Integer, tokens.IsEqual, tokens.Integer, tokens.RightBrace],
+        "$ abc": [],
+        "a $abc \n a": [tokens.Identifier, tokens.Identifier]
     }
 
     tests_fail = ["0a", "0.a", "0o8", "@"]
